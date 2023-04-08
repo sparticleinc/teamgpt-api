f76431a201780278b29c52d97fdbcba0e8be19a0
陈晨 <373471664@qq.com>
2023-04-09 00:59:43 +0800

    fix:edit message await

--- teamgpt/endpoints/conversations.py
+++ teamgpt/endpoints/conversations.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import time
 import uuid
@@ -91,91 +92,111 @@ async def create_conversations_message(
         context_number: Union[int, None] = Query(default=5),
         user: Auth0User = Security(auth.get_user)
 ):
-    log_start_time = int(time.time())
-    # 查询gpt-key配置信息,判断是否是系统的
-    key = ''
-    org_info = await Organization.get_or_none(id=organization_id, deleted_at__isnull=True)
-    if org_info is None:
-        raise HTTPException(status_code=404, detail='Organization not found')
-    if org_info.gpt_key_source is GptKeySource.SYSTEM:
-        sys_gpt_key = await SysGPTKey.get_or_none(deleted_at__isnull=True)
-        if sys_gpt_key is None:
-            raise HTTPException(status_code=404, detail='SYS GPT key not found')
-        key = sys_gpt_key.key
-    else:
-        gpt_key = await GPTKey.get_or_none(organization_id=organization_id, deleted_at__isnull=True)
-        if gpt_key is None:
-            raise HTTPException(status_code=404, detail='GPT key not found')
-        key = gpt_key.key
-    user_info = await User.get_or_none(user_id=user.id, deleted_at__isnull=True)
-    message_log = []
-    # 判断是否存在会话,没有先创建会话
-    conversation_obj = await Conversations.get_or_none(id=conversation_id, deleted_at__isnull=True)
-    if conversation_obj is None:
-        if len(conversations_input_list) > 1:
-            new_obj = await Conversations.create(id=uuid.UUID(conversation_id), user=user_info, title=title,
-                                                 organization_id=organization_id,
-                                                 model=model)
-            conversation_id = new_obj.id
+    async def async_ask(conversations_input_list, organization_id, conversation_id, title, model, context_number,
+                        user):
+        log_start_time = int(time.time())
+        # 查询gpt-key配置信息,判断是否是系统的
+        key = ''
+        org_info = await Organization.get_or_none(id=organization_id, deleted_at__isnull=True)
+        if org_info is None:
+            raise HTTPException(status_code=404, detail='Organization not found')
+        if org_info.gpt_key_source is GptKeySource.SYSTEM:
+            sys_gpt_key = await SysGPTKey.get_or_none(deleted_at__isnull=True)
+            if sys_gpt_key is None:
+                raise HTTPException(status_code=404, detail='SYS GPT key not found')
+            key = sys_gpt_key.key
         else:
-            raise HTTPException(status_code=400, detail='Conversation format error')
-    else:
-        con_obj = await Conversations.get_or_none(id=conversation_id, deleted_at__isnull=True)
-        if con_obj is None:
-            raise HTTPException(status_code=404, detail='Conversation not found')
-        model = con_obj.model
-
-    # 循环消息插入数据库
-    for conversations_input in conversations_input_list:
-        await ConversationsMessage.create(id=uuid.UUID(str(conversations_input.id)), user=user_info,
-                                          conversation_id=conversation_id,
-                                          message=conversations_input.message,
-                                          author_user=conversations_input.author_user,
-                                          content_type=conversations_input.content_type,
-                                          key=key,
-                                          )
-    # 查询前5条消息
-    con_org = await ConversationsMessage.filter(user=user_info, conversation_id=conversation_id,
-                                                deleted_at__isnull=True).order_by('-created_at').limit(context_number)
-    for con in con_org:
-        message_log.append({'role': con.author_user, 'content': con.message})
-    log_end_time = int(time.time())
-    log_run_time = log_end_time - log_start_time
-    print(log_run_time, "log_run_time")
-    # 发送sse请求数据
-    start_time = int(time.time())
-
-    async def send_gpt():
-        message = ''
-        new_msg_obj_id = ''
-        agen = ask(key, message_log[::-1], model, conversation_id)
-        async for event in agen:
-            event_data = json.loads(event['data'])
-            if event_data['sta'] == 'run':
-                message = message + event_data['message']
-                if new_msg_obj_id == '':
-                    new_msg_obj = await ConversationsMessage.create(user=user_info, conversation_id=conversation_id,
-                                                                    message=message,
-                                                                    author_user=AutherUser.ASSISTANT,
-                                                                    content_type=ContentType.TEXT,
-                                                                    key=key,
-                                                                    )
-                    new_msg_obj_id = str(new_msg_obj.id)
-                event_data['msg_id'] = new_msg_obj_id
-                event['data'] = json.dumps(event_data)
-                yield event
+            gpt_key = await GPTKey.get_or_none(organization_id=organization_id, deleted_at__isnull=True)
+            if gpt_key is None:
+                raise HTTPException(status_code=404, detail='GPT key not found')
+            key = gpt_key.key
+        user_info = await User.get_or_none(user_id=user.id, deleted_at__isnull=True)
+        message_log = []
+        # 判断是否存在会话,没有先创建会话
+        conversation_obj = await Conversations.get_or_none(id=conversation_id, deleted_at__isnull=True)
+        if conversation_obj is None:
+            if len(conversations_input_list) > 1:
+                new_obj = await Conversations.create(id=uuid.UUID(conversation_id), user=user_info, title=title,
+                                                     organization_id=organization_id,
+                                                     model=model)
+                conversation_id = new_obj.id
             else:
-                end_time = int(time.time())
-                await ConversationsMessage.filter(id=new_msg_obj_id).update(message=message,
-                                                                            run_time=end_time - start_time)
-                await GptChatMessage.create(in_message=json.dumps(message_log, ensure_ascii=False), out_message=message,
-                                            key=key,
-                                            user=user_info,
-                                            organization_id=org_info.id, conversation_id=conversation_id, )
-                yield event
-                await agen.aclose()
+                raise HTTPException(status_code=400, detail='Conversation format error')
+        else:
+            con_obj = await Conversations.get_or_none(id=conversation_id, deleted_at__isnull=True)
+            if con_obj is None:
+                raise HTTPException(status_code=404, detail='Conversation not found')
+            model = con_obj.model
 
-    return EventSourceResponse(send_gpt())
+        # 循环消息插入数据库
+        for conversations_input in conversations_input_list:
+            if conversations_input.id is None:
+                await ConversationsMessage.create(user=user_info, conversation_id=conversation_id,
+                                                  message=conversations_input.message,
+                                                  author_user=conversations_input.author_user,
+                                                  content_type=conversations_input.content_type,
+                                                  key=key,
+                                                  )
+            else:
+                await ConversationsMessage.create(id=uuid.UUID(str(conversations_input.id)), user=user_info,
+                                                  conversation_id=conversation_id,
+                                                  message=conversations_input.message,
+                                                  author_user=conversations_input.author_user,
+                                                  content_type=conversations_input.content_type,
+                                                  key=key,
+                                                  )
+        # 查询前5条消息
+        con_org = await ConversationsMessage.filter(user=user_info, conversation_id=conversation_id,
+                                                    deleted_at__isnull=True).order_by('-created_at').limit(
+            context_number)
+        for con in con_org:
+            message_log.append({'role': con.author_user, 'content': con.message})
+        log_end_time = int(time.time())
+        log_run_time = log_end_time - log_start_time
+        print(log_run_time, "log_run_time")
+        # 发送sse请求数据
+        start_time = int(time.time())
+
+        async def send_gpt():
+            message = ''
+            new_msg_obj_id = ''
+            results = ask(key, message_log[::-1], model, conversation_id)
+            async for event in results:
+                event_data = json.loads(event['data'])
+                if event_data['sta'] == 'run':
+                    message = message + event_data['message']
+                    if new_msg_obj_id == '':
+                        new_msg_obj = await ConversationsMessage.create(user=user_info, conversation_id=conversation_id,
+                                                                        message=message,
+                                                                        author_user=AutherUser.ASSISTANT,
+                                                                        content_type=ContentType.TEXT,
+                                                                        key=key,
+                                                                        )
+                        new_msg_obj_id = str(new_msg_obj.id)
+                    event_data['msg_id'] = new_msg_obj_id
+                    event['data'] = json.dumps(event_data)
+                    yield event
+                else:
+                    end_time = int(time.time())
+                    await ConversationsMessage.filter(id=new_msg_obj_id).update(message=message,
+                                                                                run_time=end_time - start_time)
+                    await GptChatMessage.create(in_message=json.dumps(message_log, ensure_ascii=False),
+                                                out_message=message,
+                                                key=key,
+                                                user=user_info,
+                                                organization_id=org_info.id, conversation_id=conversation_id, )
+                    yield event
+                    await results.aclose()
+
+        return EventSourceResponse(
+            send_gpt())
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(async_ask(conversations_input_list, organization_id, conversation_id, title, model,
+                                      context_number, user))
+    response = await task
+
+    return response
 
 
 # get conversations message
