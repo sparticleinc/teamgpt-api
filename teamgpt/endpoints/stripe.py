a249f39df64d1b0a7d96a1cb3b7a70e850a2088d
leo <leo.chen@sparticle.com>
2023-04-25 19:40:58 +0800

    fix:user first

--- teamgpt/endpoints/stripe.py
+++ teamgpt/endpoints/stripe.py
@@ -266,6 +266,8 @@ async def org_payment_plan(org_obj: Organization) -> OrgPaymentPlanOut:
 async def payment_plan(org_input: Union[PaymentPlanInt] = None, user: Auth0User = Security(auth.get_user)):
     if org_input is None:
         user_info = await User.get_or_none(user_id=user.id, deleted_at__isnull=True)
+        if user_info is None:
+            return None
         if user_info.current_organization is None or user_info.current_organization is '':
             return None
         org_obj = await Organization.get_or_none(id=user_info.current_organization, deleted_at__isnull=True)
