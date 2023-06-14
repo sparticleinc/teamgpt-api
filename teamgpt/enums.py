eb11cf5b9ea3547d94bb3b5ab7fa9a18afec6f72
陈晨 <373471664@qq.com>
2023-06-14 16:33:37 +0800

    feat:add gpt-3.5-turbo-16k

--- teamgpt/enums.py
+++ teamgpt/enums.py
@@ -23,7 +23,9 @@ class GptModel(str, Enum):
     GPT3 = 'gpt-3'
     GPT3TURBO = 'gpt-3.5-turbo'
     GPT4 = 'gpt-4'
-
+    GPT3TURBO0613 = 'gpt-3.5-turbo-0613'
+    GPT3TURBO_16K_0613 = 'gpt-3.5-turbo-16k-0613'
+    GPT3TURBO_16K = 'gpt-3.5-turbo-16k'
 
 class GptKeySource(str, Enum):
     SYSTEM = 'system'
